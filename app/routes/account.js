4c89f0948058af2986f9f1f4d3bd904dd8bf814d
Deepan Aiyasamy <deepan.cts@gmail.com>
2016-05-08 20:22:00 +0100

    Update account.js

--- app/routes/account.js
+++ app/routes/account.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model:function(){
+	  debugger;
+	  if (1 == true) {return 1;}
 		return [{id:"374673737",address:"2,Kington road, Staines"},{id:"374673787",address:"13,Kington road, Staines"}];
 	}
 });
